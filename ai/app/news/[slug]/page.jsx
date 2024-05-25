9012e07ae1b1c2fc7cd452aea35ddc8dfcf90a64
Renzaki <renzakithakuri@gmail.com>
2024-05-25 21:41:41 +0545

    adding main-header & content to news page

---
+++ ai/app/news/[slug]/page.jsx
@@ -0,0 +1,20 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import React from "react";
+
+const NewsDetail = ({ params }) => {
+  //getting url path via params.fileName
+  const newsSlug = params.slug;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
+  );
+};
+
+export default NewsDetail;
