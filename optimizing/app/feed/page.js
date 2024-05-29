8ed3b413ce37af5a3d76c6c6ef074eb428b042a2
Renzaki <renzakithakuri@gmail.com>
2024-05-29 15:05:23 +0545

    image optimization in cloudinary and metadata

--- optimizing/app/feed/page.js
+++ optimizing/app/feed/page.js
@@ -1,6 +1,16 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// data object porp received auto which will hold useful info
+export async function generateMetadata(){
+const post = await getPosts()
+const numberOfPosts = post.length
+return {
+  title:`Browse all our ${numberOfPosts} posts`,
+  description:"Browse all our posts"
+}
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
--- optimizing/app/layout.js
+++ optimizing/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/header';
 import './globals.css';
 
+// willbe by default set as metadata in pages which doesnt have custom defined metadata
 export const metadata = {
   title: 'NextPosts',
   description: 'Browse and share amazing posts.',
